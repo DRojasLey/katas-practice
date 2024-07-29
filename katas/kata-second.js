5d176ca71a57b54125d40e3b6e54f5b9da744fa8
dfrojasley <dfrojas.feld@gmail.com>
2024-07-29 13:43:11 -0500

    Failed attempt :C

--- katas/kata-second.js
+++ katas/kata-second.js
@@ -11,8 +11,10 @@
 
 function spinWords(string){
   //TODO)
-  
-  
+   //declaring reverse string helper function
+   const reverseString = (str) => {
+    return str.split("").reverse().join("");
+  }  
   //We must take the string and check for spaces that would give us the separation of words and then put them in an array
   let myString = string ;
   let onlyWords = myString.split(' ');
@@ -20,10 +22,7 @@ function spinWords(string){
   
   //if we get a single element in the array we apply a simplified lenght check
   if (onlyWords.length === 1){
-    //declaring reverse string helper function
-    const reverseString = (str) => {
-      return str.split("").reverse().join("");
-    }
+   
     if (onlyWords[0].length >= 5){
       //if the string.length > 5 then we reverse the string and return the string
       //reverse the string
@@ -34,21 +33,20 @@ function spinWords(string){
       //if the string.length < 5 then we convert the array back to a string
       onlyWords = onlyWords.join();
       // and return the string
-      return onlyWords
+      return onlyWords;
     }
   } else {
-  /*if we get more than 1 letter we proceed with further checks
+  /*if we get more than 1 word we proceed with further checks
    count the number of letters for each element in the array */
-   wordsLetterCount = onlyWords.map((x) => x.length); //<< this is now an array that contains the letter count for each element
-   // we should now apply the reversal function to any onlyWords element which length is equal or longer than 5 from documentation we should be using a for loop.
-   // and rewrite it 
-   onlyWords.array.forEach(element => {
-    
+   onlyWords = onlyWords.map((x) => {  //<< we apply to each array element (x) the following code
+     if (x.length >= 5){ //<<< if the length of the element is equal or greater than 5 we use the reverse function
+      reverseString(x);
+    }
    });
-  
-  // Once the reverse operation is completed we should then convert the array back to a string, adding the spaces 
-  
-  // finally we return the string
+    // at this point onlyWords is an array that contains any word with more than 5 letters reversed we should then convert the array back to a string, 
+    return onlyWords.join();
+     console.log(onlyWords.join());
+    // finally we return the string
   }
 }
 
