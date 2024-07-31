6c00c83367441e32adc37ec32f9535539f7b00f5
dfrojasley <dfrojas.feld@gmail.com>
2024-07-30 23:37:49 -0500

    Completed fourth Kata and updated README

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ Simple repository to complete Katas in Codewars
 |Multiples of 3 or 5 | [kata-first.js](katas/kata-first.js) | 45 min| Pass   | https://shorturl.at/0tkiT |
 |Stop gninnipS My sdroW! | [kata-second.js](katas/kata-second.js) | 80 min | failed | https://shorturl.at/f2fx3 |
 |Vowel Count | [kata-third.js](katas/kata-third.js) | 15 min | Pass | https://shorturl.at/RyenV |
+|Are You Playing Banjo? | [kata-fourth.js](katas/kata-fourth.js) | 10 min | Pass | https://shorturl.at/E3NXY |
 
 
 >Many thanks to my mentor: Daniel Trujillo (GitHub: https://github.com/danieltrujillo003)
---
+++ katas/kata-fourth.js
@@ -0,0 +1,35 @@
+/* The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+Names given are always valid strings.
+
+*/
+
+function areYouPlayingBanjo(name) {
+//check name initial:
+    const lasErres = [82,114];
+
+  if (lasErres.includes(name.charCodeAt(0))){
+        return `${name} plays banjo`;
+  } else {
+    return `${name} does not play banjo`
+  }
+  }
+  /* test examples:
+
+  const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(areYouPlayingBanjo("Adam"), "Adam does not play banjo");
+    assert.strictEqual(areYouPlayingBanjo("Paul"), "Paul does not play banjo");
+    assert.strictEqual(areYouPlayingBanjo("Ringo"), "Ringo plays banjo");
+    assert.strictEqual(areYouPlayingBanjo("bravo"), "bravo does not play banjo");
+    assert.strictEqual(areYouPlayingBanjo("rolf"), "rolf plays banjo");
+    })
+  })
+
+  */
