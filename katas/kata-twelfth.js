243d7f8bbc81745f4b3099a876dfb2c910449c20
dfrojasley <dfrojas.feld@gmail.com>
2024-08-07 14:35:14 -0500

    first solution I found for this challenge

--- katas/kata-twelfth.js
+++ katas/kata-twelfth.js
@@ -11,7 +11,34 @@ Examples:
 
 
 
+
+// THE OVERCOMLICATED WAY I SHOOT MYSELF IN THE FOOT:
+
 function solution(str){
+    // I don't think I need this much variables, and I think most of the complexity on the below code comes from the amount of variables I used
+    let processStr = '';
+    let resultArray = [];
+    let temporaryString = '';
+    processStr = str.split('');
+    // this can be a ternary to check for the oddness or eveness of the characters
+    if (processStr.length % 2 != 0){
+        processStr.push('_');
+    }
+    // this function declaration is so unnecesary it hurts me, but I was running out of time so I did't feel like simplifying
+    const concatenationFunction = (passedStr) => {
+        while (processStr.length != 0){
+            for (i = 0; i< 2; i++){
+                temporaryString = temporaryString.concat(processStr.shift());
+                console.log(temporaryString);
+            }
+            resultArray.push(temporaryString);
+            console.log(resultArray)
+            temporaryString = '';
+            console.log(temporaryString + 'despues de vaciar');
+        }
+    }
+    concatenationFunction(processStr);
+    return resultArray;
 
 }
 
