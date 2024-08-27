5087e0e0630a4f5b534cf307a52125ec20a3e401
dfrojasley <dfrojas.feld@gmail.com>
2024-08-27 01:28:15 -0500

    code style edits

--- katas/32-kata/32-kata.js
+++ katas/32-kata/32-kata.js
@@ -1,12 +1,8 @@
 function accum(s) {
     let result = '';
     s = s.split('');
-    console.log(s)
     for (let i = 0 ; i < s.length ; i++){
-
         result += s[i].toUpperCase() + s[i].toLowerCase().repeat(i) + '-'
     }
     return result.slice(0,-1)
 }
-
-
